e68bfd709334508a87dad0f8adccebff44dc54c6
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2025-04-09 16:03:47 +0530

    trying to solve database migration missing error

--- blog/blog/wsgi.py
+++ blog/blog/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+# Add this block to run migrations on startup
+from django.core.management import call_command
+try:
+    call_command('migrate')
+except Exception as e:
+    print(f"Error running migrations: {e}")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
